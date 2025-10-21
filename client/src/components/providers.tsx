bc54c99399b554bc58c3a56ab02455f88e367e93
hrishihunde <hrishixhrishi@gmail.com>
2025-10-21 17:09:19 +0530

    setup: providers

---
+++ client/src/components/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { WagmiProvider } from "wagmi";
+import { Query, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { config } from "../../lib/wagmi/config";
+import { useState } from "react";
+import { ThemeProvider } from "./theme-provider";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
+}
